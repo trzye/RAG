fec61795ed8e904850788aec34c134a6a67438da
trzye <shafear100@gmail.com>
2016-06-11 16:14:51 +0200

    refaktoring i dodanie wsparcia dla serwerów

--- rag_dis.py
+++ rag_dis.py
@@ -3,8 +3,7 @@
 import argparse
 import random
 import math
-import pp
-
+# import pp
 
 # Wypisuje instrukcję do programu
 def usage():
@@ -25,7 +24,8 @@ def parse_parameters():
     parser.add_argument('-w', type=str, default="wlp", choices=["wlp", "wlr", "wd"], help="sposób wyboru: wlp, wlr, wd")
     parser.add_argument("-m", type=float, default=0.25, help="współczynnik mutacji <0,1>")
     parser.add_argument("-k", type=float, default=0.25, help="współczynnik krzyżowania <0,1>")
-    parser.add_argument("-t", type=int, default=2, help="ilość wątków")
+    parser.add_argument("-t", type=int, default=1, help="liczba wątków")
+    parser.add_argument("-x", type=str, default=None, help="server np: 192.169.0.1:1234")
     # opcjonalne argumenty
     parser.add_argument('-s', type=str, choices=["lin", "pot", "log"], help="sposób skalowania: lin, pot, log")
     parser.add_argument('-n', type=str, help="nazwa pliku do zapisania informacji o przebiegu generacji")
@@ -47,10 +47,10 @@ def scale(number, minimum, maximum):
 
 
 # Skalowanie jednego osobnika
-def scale_specimen(specimen, scale_minimum, scale_maximum):
+def scale_specimen(specimen, scale_min, scale_max):
     scaled_specimen = [None] * len(specimen)
     for i in range(len(specimen)):
-        scaled_specimen[i] = scale(specimen[i], scale_minimum[i], scale_maximum[i])
+        scaled_specimen[i] = scale(specimen[i], scale_min[i], scale_max[i])
     return scaled_specimen
 
 
@@ -71,32 +71,61 @@ def get_scale_max(f):
 
 
 # Oblicza funkcję celu
-def objective_function(f_args, f, scale_minimum, scale_maximum):
+def objective_function(f_args, f, scale_min, scale_max):
     if f == "f1":
-        a = scale(f_args[0], scale_minimum[0], scale_maximum[0])
-        b = scale(f_args[1], scale_minimum[1], scale_maximum[1])
-        c = scale(f_args[2], scale_minimum[2], scale_maximum[2])
+        a = scale(f_args[0], scale_min[0], scale_max[0])
+        b = scale(f_args[1], scale_min[1], scale_max[1])
+        c = scale(f_args[2], scale_min[2], scale_max[2])
         return 4 * a + 6 * c - 15 * pow(a, 2) - 20 * pow(b, 2) - 6 * pow(a, 3) - 70
     if f == "f2":
-        a = scale(f_args[0], scale_minimum[0], scale_maximum[0])
-        b = scale(f_args[1], scale_minimum[1], scale_maximum[1])
-        c = scale(f_args[2], scale_minimum[2], scale_maximum[2])
+        a = scale(f_args[0], scale_min[0], scale_max[0])
+        b = scale(f_args[1], scale_min[1], scale_max[1])
+        c = scale(f_args[2], scale_min[2], scale_max[2])
         return 4 * math.cos(a) + 6 * math.cos(c) - 20 * pow(b, 2) + 3 * pow(a, 3) - 70
 
 
-# Uruchomienie programu
-if __name__ == '__main__':
-    args = parse_parameters()
-    # TODO: lepsza walidacja parametrów wejściowych
-    if args.f is None:
-        usage()
+# Oblicza wyniki (funkcję celu) dla osobników. (Przetwarzanie równoległe)
+def calculate_results_parallel(specimens, args, scale_min, scale_max):
+    res = [None] * args.o
+
+    if args.x is None:
+        ppservers = ()
     else:
-        # TODO: lepsze wypisanie parametrów wywołania
+        ppservers = (args.x,)
+    job_server = pp.Server(args.t, ppservers=ppservers)
+
+    jobs = []
+    for i in range(len(res)):
+        jobs.append(
+            job_server.submit(objective_function, (specimens[i], args.f, scale_min, scale_max),
+                              (scale,), ('math',)))
+
+    i = 0
+    for job in jobs:
+        res[i] = job()
+        i += 1
+
+    # wypisanie informacji dla wyników danego pokolenia
+    job_server.print_stats()
+
+    return res
+
+
+# Oblicza wyniki (funkcję celu) dla osobników. (Przetwarzanie równoległe)
+def calculate_results(specimens, args, scale_min, scale_max):
+    res = [None] * arguments.o
+    for actual_specimen in range(len(res)):
+                res[actual_specimen] = objective_function(specimens[actual_specimen], args.f, scale_min, scale_max)
+    return res
+
+
+# Uruchomienie programu
+def main(args):
+    # TODO: lepsze wypisanie parametrów wywołania
         print("Parametry wywołania: " + str(args))
 
         # przygotowanie danych
         specimens = [None] * args.o
-        results = [None] * args.o
         best_specimen = None
         best_result = None
         scale_min = get_scale_min(args.f)
@@ -110,28 +139,16 @@ if __name__ == '__main__':
         for actual_generation in range(1, args.p + 1):
             print("Pokolenie: " + str(actual_generation))
 
-            # przetwarzanie równoległe
-            ppservers = ()
-            job_server = pp.Server(args.t, ppservers=ppservers)
-
-            # obliczam funkcję celu - równoleglei
-	    jobs = []
-	    for actual_specimen in range(len(specimens)):
-                jobs.append(job_server.submit(objective_function, (specimens[actual_specimen], args.f, scale_min, scale_max), (scale,), ('math',)))
-
-            i = 0
-	    for job in jobs:
-		results[i] = job()
-                i=i+1
-            
-            #czekam na wykonanie zadania
-            # job_server.wait()
+            # obliczam funkcję celu
+            if args.t > 1:
+                results = calculate_results_parallel(specimens, args, scale_min, scale_max)
+            else:
+                results = calculate_results(specimens, args, scale_min, scale_max)
 
             # najlepsze wyniki generacji
             best_generation_result = min(results)
             best_generation_specimen = specimens[results.index(best_generation_result)]
 
-            # wypisanie informacji dla wyników danego pokolenia
             print("\tNajlepszy osobnik w pokoleniu: {0}\n\tNajlepszy wynik w pokoleniu: {1}".format(
                 str(scale_specimen(best_generation_specimen, scale_min, scale_max)), best_generation_result))
 
@@ -152,3 +169,11 @@ if __name__ == '__main__':
         print("Wyniki")
         print("\tNajlepszy osobnik: {0}\n\tNajlepszy wynik: {1}".format(
             str(scale_specimen(best_specimen, scale_min, scale_max)), best_result))
+
+
+if __name__ == '__main__':
+    arguments = parse_parameters()
+    if arguments.f is None:
+        usage()
+    else:
+        main(arguments)
