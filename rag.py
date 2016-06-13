86075bcf05b62034c79261746bf91f5ac4c218dd
trzye <shafear100@gmail.com>
2016-06-13 15:27:01 +0200

    ok

--- rag.py
+++ rag.py
@@ -74,7 +74,7 @@ def get_scale_max(f):
 
 # Oblicza funkcję celu
 def objective_function(f_args, f, scale_min, scale_max):
-    time.sleep(0.01)
+    # time.sleep(0.01)
     if f == "f1":
         a = scale(f_args[0], scale_min[0], scale_max[0])
         b = scale(f_args[1], scale_min[1], scale_max[1])
@@ -116,7 +116,7 @@ def calculate_results_parallel(specimens, args, scale_min, scale_max):
 def calculate_results(specimens, args, scale_min, scale_max):
     res = [None] * arguments.o
     for actual_specimen in range(len(res)):
-                res[actual_specimen] = objective_function(specimens[actual_specimen], args.f, scale_min, scale_max)
+        res[actual_specimen] = objective_function(specimens[actual_specimen], args.f, scale_min, scale_max)
     return res
 
 
@@ -133,7 +133,7 @@ def calculate_adaptation_function(results):
 
 
 # przeskaluj wyniki
-def calculate_scale_function(args, results):
+def calculate_scale_function(args, results, actual_generation):
     tmp_results = results
     try:
         if args.s == "lin":
@@ -141,18 +141,42 @@ def calculate_scale_function(args, results):
             average = sum(results) / float(len(results))
             maximum = max(results)
             minimum = min(results)
-            if minimum > (multiplying_factor * average - maximum)/(multiplying_factor - 1):
+            if minimum > (multiplying_factor * average - maximum) / (multiplying_factor - 1):
                 a = ((multiplying_factor - 1) * average) / (maximum - average)
-                b = average * ((maximum - multiplying_factor * average)/(maximum - average))
+                b = average * ((maximum - multiplying_factor * average) / (maximum - average))
             else:
                 a = average / (average - minimum)
-                b = -minimum * (average/(average - minimum))
+                b = -minimum * (average / (average - minimum))
             for i in range(len(results)):
                 results[i] = a * results[i] + b
             return results
-        if args.s == "pot":  # TODO skalowanie potęgowe
+        if args.s == "pot":
+            p1 = 0.05
+            p2 = 0.1
+            alfa = 0.1
+            optimal_range = 0.1
+            initial_range = sum(results) / float(len(results))
+            if initial_range < 0:
+                initial_range *= -1
+            T = args.p
+            tang_p = p2 * ((initial_range / optimal_range) ** alfa)
+            t = actual_generation  # aktualny nr pokolenia
+            to_tang = ((t / (T + 1)) * (math.pi / 2))
+            k = ((optimal_range / initial_range) ** p1) * math.tan(to_tang) ** tang_p
+            for i in range(len(results)):
+                results[i] = math.pow(results[i], k)
             return results
-        if args.s == "log":  # TODO skalowanie logarytmiczne
+        if args.s == "log":  # skalowanie logarytmiczne
+            logs = [float] * len(results)  # tablica przechowująca wartość logarytmów
+            for i in range(len(results)):
+                logs[i] = math.log10(results[i])
+            b = round(max(logs), 1)
+            if b < max(logs) or b == max(logs):
+                b += 0.1
+            else:
+                b = b
+            for j in range(len(results)):
+                results[j] = b - logs[j]
             return results
     # gdy populacja zostanie zdominowana przez te same osobniki istnieje szansa dzielenia przez zero
     # w sytuacji gdy osobniki są takie same w populacji nie ma sensu funkcja skalowania i omijamy
@@ -196,10 +220,41 @@ def choose_specimens(args, specimens, results):
         while len(new_specimens) > args.o:
             new_specimens.pop()
         return new_specimens
-    if args.w == "wlp":  # TODO: wybór losowy z powtórzeniami
-        return specimens
+    if args.w == "wlp":  # wybór losowy z powtórzeniami
+        new_specimens = []
+        r = [float] * len(specimens)
+        cumulated_probability = [float] * len(specimens)
+        while len(new_specimens) == 0:
+            for k in range(len(specimens)):
+                choose_probability[k] = results[k] / adaptation_sum
+                sum_prob = 0.0
+                r[k] = random.uniform(0, 1)
+                for z in range(k):
+                    sum_prob += choose_probability[z]
+                cumulated_probability[k] = sum_prob
+            for l in range(len(specimens)):
+                if r[l] <= cumulated_probability[1]:
+                    new_specimens.append(specimens[1])
+                else:
+                    for m in range(len(specimens)):
+                        if r[l] > cumulated_probability[m - 1] and cumulated_probability[m] and m > 1:
+                            new_specimens.append(specimens[l])
+        while len(new_specimens) < args.o:
+            new_specimens.append(new_specimens[len(new_specimens) - 1])
+        while len(new_specimens) > args.o:
+            new_specimens.pop()
+        return new_specimens
     if args.w == "wlr":  # TODO: wybór losowy według reszt bez powtórzeń
-        return specimens
+        new_specimens = []
+        sorted_specimens = [x for (y, x) in sorted(zip(choose_copies_rest, specimens))]
+        while len(new_specimens) < args.o:
+            for i in range(len(specimens)):
+                r = random.uniform(0, 1)
+                if r < choose_copies_rest[i]:
+                    new_specimens.append(sorted_specimens[i])
+                    choose_copies_rest[i] -= 1
+
+        return new_specimens
 
 
 # mutacja
@@ -228,79 +283,78 @@ def cross_specimens(args, specimens):
 
 # Uruchomienie programu
 def main(args):
+    start_time = time.time()
 
-        start_time = time.time()
+    # inicjacja pliku do zapisu
+    if args.n is not None:
+        write_file = open(args.n, 'w')
+        write_file.write("Parametry wywołania: " + str(args))
 
-        # inicjacja pliku do zapisu
+    # przygotowanie danych
+    specimens = [None] * args.o
+    best_specimen = None
+    best_result = None
+    scale_min = get_scale_min(args.f)
+    scale_max = get_scale_max(args.f)
+
+    # losowanie osobników
+    for actual_specimen in range(len(specimens)):
+        specimens[actual_specimen] = rand_specimen(args.f)
+
+    # właściwy algorytm genetyczny, pętla po pokoleniach
+    for actual_generation in range(1, args.p + 1):
         if args.n is not None:
-            write_file = open(args.n, 'w')
-            write_file.write("Parametry wywołania: " + str(args))
+            write_file.write("\nPokolenie: " + str(actual_generation))
 
-        # przygotowanie danych
-        specimens = [None] * args.o
-        best_specimen = None
-        best_result = None
-        scale_min = get_scale_min(args.f)
-        scale_max = get_scale_max(args.f)
-
-        # losowanie osobników
-        for actual_specimen in range(len(specimens)):
-            specimens[actual_specimen] = rand_specimen(args.f)
-
-        # właściwy algorytm genetyczny, pętla po pokoleniach
-        for actual_generation in range(1, args.p + 1):
-            if args.n is not None:
-                write_file.write("\nPokolenie: " + str(actual_generation))
-
-            # obliczam funkcję celu
-            if (args.t > 1) or (args.x is not None):
-                results = calculate_results_parallel(specimens, args, scale_min, scale_max)
-            else:
-                results = calculate_results(specimens, args, scale_min, scale_max)
-
-            # najlepsze wyniki generacji
-            best_generation_result = min(results)
-            best_generation_specimen = specimens[results.index(best_generation_result)]
-
-            if args.n is not None:
-                write_file.write("\n\tNajlepszy osobnik w pokoleniu: {0}\n\tNajlepszy wynik w pokoleniu: {1}\n".format(
-                    str(scale_specimen(best_generation_specimen, scale_min, scale_max)), best_generation_result))
-
-            # dodaję do globalnie najlepszych, jeżeli są lepsze
-            if best_result is None:
-                best_result = best_generation_result
-                best_specimen = best_generation_specimen
-            elif best_result > best_generation_result:
-                best_result = best_generation_result
-                best_specimen = best_generation_specimen
-
-            # oblicz na funkcję przystosowania
-            results = calculate_adaptation_function(results)
-
-            # oblicz na funkcję skalowania
-            if args.s is not None:
-                results = calculate_scale_function(args, results)
-
-            # wybierz osobniki do następnej generacji
-            specimens = choose_specimens(args, specimens, results)
-
-            # mutuj
-            specimens = mutate_specimens(args, specimens)
-
-            # krzyżuj
-            specimens = cross_specimens(args, specimens)
-
-        # wypisanie informacji dla ostatecznych wyników
-        if args.n is not None:
-            write_file.write("Wyniki\n")
-            write_file.write("\tNajlepszy osobnik: {0}\n\tNajlepszy wynik: {1}\n".format(
-                str(scale_specimen(best_specimen, scale_min, scale_max)), best_result))
-            write_file.write("Czas przetwarzania: " + str(time.time() - start_time))
+        # obliczam funkcję celu
+        if (args.t > 1) or (args.x is not None):
+            results = calculate_results_parallel(specimens, args, scale_min, scale_max)
         else:
-            print("Wyniki")
-            print("\tNajlepszy osobnik: {0}\n\tNajlepszy wynik: {1}".format(
-                str(scale_specimen(best_specimen, scale_min, scale_max)), best_result))
-            print "Czas przetwarzania: ", time.time() - start_time
+            results = calculate_results(specimens, args, scale_min, scale_max)
+
+        # najlepsze wyniki generacji
+        best_generation_result = min(results)
+        best_generation_specimen = specimens[results.index(best_generation_result)]
+
+        if args.n is not None:
+            write_file.write("\n\tNajlepszy osobnik w pokoleniu: {0}\n\tNajlepszy wynik w pokoleniu: {1}\n".format(
+                str(scale_specimen(best_generation_specimen, scale_min, scale_max)), best_generation_result))
+
+        # dodaję do globalnie najlepszych, jeżeli są lepsze
+        if best_result is None:
+            best_result = best_generation_result
+            best_specimen = best_generation_specimen
+        elif best_result > best_generation_result:
+            best_result = best_generation_result
+            best_specimen = best_generation_specimen
+
+        # oblicz na funkcję przystosowania
+        results = calculate_adaptation_function(results)
+
+        # oblicz na funkcję skalowania
+        if args.s is not None:
+            results = calculate_scale_function(args, results, actual_generation)
+
+        # wybierz osobniki do następnej generacji
+        specimens = choose_specimens(args, specimens, results)
+
+        # mutuj
+        specimens = mutate_specimens(args, specimens)
+
+        # krzyżuj
+        specimens = cross_specimens(args, specimens)
+
+    # wypisanie informacji dla ostatecznych wyników
+    if args.n is not None:
+        write_file.write("Wyniki\n")
+        write_file.write("\tNajlepszy osobnik: {0}\n\tNajlepszy wynik: {1}\n".format(
+            str(scale_specimen(best_specimen, scale_min, scale_max)), best_result))
+        write_file.write("Czas przetwarzania: " + str(time.time() - start_time))
+    else:
+        print("Wyniki")
+        print("\tNajlepszy osobnik: {0}\n\tNajlepszy wynik: {1}".format(
+            str(scale_specimen(best_specimen, scale_min, scale_max)), best_result))
+        print "Czas przetwarzania: ", time.time() - start_time
 
 
 if __name__ == '__main__':
@@ -309,4 +363,3 @@ if __name__ == '__main__':
         usage()
     else:
         main(arguments)
-
